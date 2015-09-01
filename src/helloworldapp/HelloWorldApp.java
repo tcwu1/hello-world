1b7821b1f2f14b1ef6fa2832c7000ced49e3a0d6
tiffanychen <tiffanychen@wireless1x-155-41-84-63.bu.edu>
2015-09-01 11:13:27 -0400

    new commit

--- src/helloworldapp/HelloWorldApp.java
+++ src/helloworldapp/HelloWorldApp.java
@@ -15,7 +15,7 @@ public class HelloWorldApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello world!");
     }
     
 }
